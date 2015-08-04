727c38c55df82d356e61f3be377f661cf73e922e
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2015-08-05 01:07:50 +0600

    add event handler to pics

--- dota-antipick/src/main/java/com/vsprog/anitipick/GridViewDemo.java
+++ dota-antipick/src/main/java/com/vsprog/anitipick/GridViewDemo.java
@@ -1,11 +1,14 @@
 package com.vsprog.anitipick;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -13,6 +16,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GridViewDemo extends Application {
+
+    public static final int ROW_COUNT = 7;
 
     @Override
     public void start(Stage stage) {
@@ -46,10 +51,23 @@ public class GridViewDemo extends Application {
 
         for (int i = 0; i < heroesCount; i++) {
             ImageView imageView = new ImageView(images.get(i));
+
+            final int count = i;
+            imageView.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Dialog message");
+                    alert.setContentText("This is " + heroes.get(count).getName());
+                    alert.showAndWait();
+                    event.consume();
+                }
+            });
+
             grid.add(imageView, imageColumn, imageRow);
             imageColumn++;
 
-            if (imageColumn > 7) {
+            if (imageColumn > ROW_COUNT) {
                 imageColumn = 0;
                 imageRow++;
             }
